37f713feefab9bee9f24614947ac4a683159805c
Meghan Mullally <ms.mullally27@gmail.com>
2019-06-20 09:58:37 -0700

    took out some css

--- src/views/things.vue
+++ src/views/things.vue
@@ -46,7 +46,7 @@ import VueAxios from 'vue-axios'
 import VueYouTubeEmbed from 'vue-youtube-embed'
 
 Vue.use(VueYouTubeEmbed);
-export default{
+export default {
   name: "app",
   data() {
     return {
@@ -88,38 +88,7 @@ export default{
 </script>
 
 <style>
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 
-h1,
-h2 {
-  font-weight: normal;
-}
-
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
-}
-
-// #video-container 
-//   height: 400px;
-//   width: 400px;
-// 
 
 * {
   box-sizing: border-box;
